15426a7fac3158bdc9a8a944397d658d0f9eaf86
Dennis Ries <dtries@gmail.com>
2019-06-28 12:06:57 -0600

    color swatch choices added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,180 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Row, Col } from 'react-materialize';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Row>
+          <Col s={2} className="r1" id="r1c0"> 
+          Cold North &nbsp;
+          </Col> 
+          <Col s={2} className="r1" id="r1c1">
+            1
+          </Col>
+          <Col s={2} className="r1" id="r1c2">
+            2
+          </Col>
+          <Col s={2} className="r1" id="r1c3">
+            3
+          </Col>
+          <Col s={2} className="r1" id="r1c4">
+            4
+          </Col>
+          <Col s={2} className="r1" id="r1c5">
+            5
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={2} className="r2" id="r2c0"> 
+          Bright Abstract &nbsp;
+          </Col>           
+          <Col s={2} className="r2" id="r2c1">
+            1
+          </Col>
+          <Col s={2} className="r2" id="r2c2">
+            2
+          </Col>
+          <Col s={2} className="r2" id="r2c3">
+            3
+          </Col>
+          <Col s={2} className="r2" id="r2c4">
+            4
+          </Col>
+          <Col s={2} className="r2" id="r2c5">
+            5
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={2} className="r3" id="r3c0"> 
+          Ink in Water &nbsp;
+          </Col>
+          <Col s={2} className="r3" id="r3c1">
+            1
+          </Col>
+          <Col s={2} className="r3" id="r3c2">
+            2
+          </Col>
+          <Col s={2} className="r3" id="r3c3">
+            3
+          </Col>
+          <Col s={2} className="r3" id="r3c4">
+            4
+          </Col>
+          <Col s={2} className="r3" id="r3c5">
+            5
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={2} className="r4" id="r4c0"> 
+          Color Powder &nbsp;
+          </Col>           
+          <Col s={2} className="r4" id="r4c1">
+            1
+          </Col>
+          <Col s={2} className="r4" id="r4c2">
+            2
+          </Col>
+          <Col s={2} className="r4" id="r4c3">
+            3
+          </Col>
+          <Col s={2} className="r4" id="r4c4">
+            4
+          </Col>
+          <Col s={2} className="r4" id="r4c5">
+            5
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={2} className="r5" id="r5c0"> 
+          Stockholm &nbsp;
+          </Col>           
+          <Col s={2} className="r5" id="r5c1">
+            1
+          </Col>
+          <Col s={2} className="r5" id="r5c2">
+            2
+          </Col>
+          <Col s={2} className="r5" id="r5c3">
+            3
+          </Col>
+          <Col s={2} className="r5" id="r5c4">
+            4
+          </Col>
+          <Col s={2} className="r5" id="r5c5">
+            5
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={2} className="r6" id="r6c0"> 
+          Life Music &nbsp;
+          </Col>           
+          <Col s={2} className="r6" id="r6c1">
+            1
+          </Col>
+          <Col s={2} className="r6" id="r6c2">
+            2
+          </Col>
+          <Col s={2} className="r6" id="r6c3">
+            3
+          </Col>
+          <Col s={2} className="r6" id="r6c4">
+            4
+          </Col>
+          <Col s={2} className="r6" id="r6c5">
+            5
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={2} className="r7" id="r7c0"> 
+          Bright Socks &nbsp;
+          </Col>           
+          <Col s={2} className="r7" id="r7c1">
+            1
+          </Col>
+          <Col s={2} className="r7" id="r7c2">
+            2
+          </Col>
+          <Col s={2} className="r7" id="r7c3">
+            3
+          </Col>
+          <Col s={2} className="r7" id="r7c4">
+            4
+          </Col>
+          <Col s={2} className="r7" id="r7c5">
+            5
+          </Col>
+        </Row>
+
+        <Row> 
+          <Col s={2} className="r8" id="r8c0"> 
+          Verzini &nbsp;
+          </Col>           
+          <Col s={2} className="r8" id="r8c1">
+            1
+          </Col>
+          <Col s={2} className="r8" id="r8c2">
+            2
+          </Col>
+          <Col s={2} className="r8" id="r8c3">
+            3
+          </Col>
+          <Col s={2} className="r8" id="r8c4">
+            4
+          </Col>
+          <Col s={2} className="r8" id="r8c5">
+            5
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
